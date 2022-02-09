e7cce399eb8c99960bfd901d090cc923daf84088
schimen <einesimen@gmail.com>
2022-02-09 18:04:52 +0100

    client.c, server.c: create basic csp server - client example

--- client.c
+++ client.c
@@ -1,69 +1,145 @@
-/*
-    Client demo program
-    source: https://docs.kubos.com/1.2.0/apis/libcsp/csp_docs/example.html
-*/
+#include <stdio.h>
+#include <stdlib.h>
 
 #include <csp/csp.h>
-#include <csp/interfaces/csp_if_can.h>
+#include <csp/drivers/usart.h>
+#include <csp/interfaces/csp_if_kiss.h>
+#include <csp/arch/csp_thread.h>
 
-/*---------------------------Setup-------------------------------------------*/
+// server address must be global for client task to read it:
+static uint8_t server_address;
 
-/* CAN configuration struct for SocketCAN interface "can0" */
-struct csp_can_config can_conf = {.ifc = "can0"};
+CSP_DEFINE_TASK(task_client) {
+    // pointer to connection and packet
+    csp_packet_t * packet;
+	csp_conn_t   * conn;
 
-/* Init buffer system with 10 packets of maximum 320 bytes each */
-csp_buffer_init(10, 320);
+	while (1) {
+        csp_sleep_ms(2000);
+        
+        // ping server address and show the response time:
+        
+        printf("Pinging address %d: ", server_address);
+		int result = csp_ping(server_address, 100, 1, CSP_O_NONE); // ping with 1 byte
+		printf("Ping result %d [ms]\r\n", result);
+        
+        csp_sleep_ms(2000);
+        
+        // send a packet to the server:
+        
+        // get a packet-buffer of 100 bytes
+		packet = csp_buffer_get(100);
+        // connect to server (on port 10)
+        conn = csp_connect(CSP_PRIO_NORM, server_address, 10, 1000, CSP_O_NONE);
+		// copy message to packet and set packet length
+		const char *msg = "This message was sent over KISS";
+		strcpy((char *) packet->data, msg);
+		packet->length = strlen(msg);
+		// send packet
+		csp_send(conn, packet, 1000);
+        printf("Sent '%s' to address %d\n", msg, csp_conn_dst(conn));
+        // close connection after sending
+		csp_close(conn);
+	}
+	return CSP_TASK_RETURN;
+}
 
-/* Init CSP with address 2 */
-csp_init(2);
+void csp_setup( uint8_t address ) {
+    printf("Setting up CSP with address: %d\n", address);
+    
+    // csp configuration and initialization
+    csp_conf_t csp_conf;
+    csp_conf_get_defaults(&csp_conf);
+    csp_conf.address = address;
+    csp_init(&csp_conf);
 
-/* Init the CAN interface with hardware filtering */
-csp_can_init(CSP_CAN_MASKED, &can_conf)
+    // initialize buffer
+    csp_buffer_init(5, 300);
+}
 
-/* Setup default route to CAN interface */
-csp_route_set(CSP_DEFAULT_ROUTE, &csp_can_tx, CSP_HOST_MAC);
+void kiss_setup( char              * device
+               , char              * name
+               , csp_iface_t       * kiss_iface_p
+               , csp_kiss_handle_t * kiss_driver_p
+               , usart_callback_t    callback_func ) {
+    printf("Setting up KISS interface %s with device: %s\n", name, device);
 
-/* Start router task with 500 word stack, OS task priority 1 */
-csp_route_start_task(500, 1);
+    // usart configuration
+    struct usart_conf conf;
+    conf.device = device;
+    conf.baudrate = 115200;
+    
+    usart_init(&conf);
 
+    csp_kiss_init(kiss_iface_p, kiss_driver_p, usart_putc,
+                  usart_insert, name);
+    
+    // function to catch incoming chars. 
+    usart_set_callback(callback_func);
 
-/*---------------------------Client------------------------------------------*/
-int send_packet(void) {
+    // set route for kiss interface
+    csp_route_set(CSP_DEFAULT_ROUTE, kiss_iface_p, CSP_NODE_MAC);
+}
 
-    /* Get packet buffer for data */
-    csp_packet_t *packet = csp_buffer_get(data_size);
-    if (packet == NULL) {
-        /* Could not get buffer element */
-        printf("Failed to get buffer element\\n");
-        return -1;
+void csp_start( csp_thread_return_t (* thread_task)(void *), char * name ) {
+    printf("Starting %s task\n", name);
+
+    // start router task with 1000 bytes of stack    
+    csp_route_start_task(1000, 0);
+
+    // create the thread with our task
+    csp_thread_handle_t thread_handle;
+    csp_thread_create(thread_task, name, 1000, NULL, 0, &thread_handle);
+}
+
+void show_debug() {
+    printf("Debug enabled\r\n");
+	csp_debug_toggle_level(3);
+	csp_debug_toggle_level(4);
+
+	printf("Conn table\r\n");
+	csp_conn_print_table();
+
+	printf("Route table\r\n");
+	csp_route_print_table();
+
+	printf("Interfaces\r\n");
+	csp_route_print_interfaces();
+}
+
+int main(int argc, char * argv[]) {
+    // parse command line arguments
+    uint8_t address;
+    char * kiss_device;
+    if (argc == 4) {
+        address = atoi(argv[1]);
+        kiss_device = argv[2];
+        server_address = atoi(argv[3]);
     }
-
-    /* Connect to host HOST, port PORT with regular UDP-like protocol and 1000 ms timeout */
-    csp_conn_t *conn = csp_connect(CSP_PRIO_NORM, HOST, PORT, 1000, CSP_O_NONE);
-    if (conn == NULL) {
-        /* Connect failed */
-        printf("Connection failed\\n");
-        /* Remember to free packet buffer */
-        csp_buffer_free(packet);
-        return -1;
+    else  {
+        printf("Usage:\n"
+               " client <client-csp-id> <kiss-device> <server-csp-id>\n");
+       exit(1);
     }
-
-    /* Copy message to packet */
-    char *msg = "HELLO";
-    strcpy(packet->data, msg);
-
-    /* Set packet length */
-    packet->length = strlen(msg);
-
-    /* Send packet */
-    if (!csp_send(conn, packet, 1000)) {
-        /* Send failed */
-        printf("Send failed\\n");
-        csp_buffer_free(packet);
+    
+    csp_setup(address);
+    
+    // initialize kiss interface (these values must be available for all of main function)
+    csp_iface_t kiss_iface;
+    csp_kiss_handle_t kiss_driver;
+    // callback function when receiving over kiss
+    void kiss_callback_func(uint8_t * buf, int len, void * pxTaskWoken) {
+        csp_kiss_rx(&kiss_iface, buf, len, pxTaskWoken);
     }
+    // setup kiss
+    kiss_setup(kiss_device, "KISS interface", &kiss_iface, &kiss_driver, kiss_callback_func);
 
-    /* Close connection */
-    csp_close(conn);
+    csp_start(task_client, "client");
+    
+    //show_debug(); // uncomment this line for debug info
 
-    return 0
+    while(1) {
+    	csp_sleep_ms(1000);
+    }
+    return 0;
 }
--- server.c
+++ server.c
@@ -1,67 +1,145 @@
-/*
-    Server demo program
-    source: https://docs.kubos.com/1.2.0/apis/libcsp/csp_docs/example.html
-*/
+#include <stdio.h>
+#include <stdlib.h>
 
 #include <csp/csp.h>
-#include <csp/interfaces/csp_if_can.h>
+#include <csp/drivers/usart.h>
+#include <csp/interfaces/csp_if_kiss.h>
+#include <csp/arch/csp_thread.h>
 
-/*---------------------------Setup-------------------------------------------*/
+CSP_DEFINE_TASK(task_server) {
+        // create socket, bind it to all ports and create a queue for 10 connections
+        csp_socket_t * sock = csp_socket(CSP_SO_NONE);
+        csp_bind(sock, CSP_ANY);
+        csp_listen(sock, 10);
+    
+        // pointer to current connection and packet
+        csp_conn_t   * conn;
+        csp_packet_t * packet;
 
-/* CAN configuration struct for SocketCAN interface "can0" */
-struct csp_can_config can_conf = {.ifc = "can0"};
+        // process incoming connections
+        while (1) {
+                // wait for connection, 1 s timeout
+                if ((conn = csp_accept(sock, 10000)) == NULL)
+                        continue;
 
-/* Init buffer system with 10 packets of maximum 320 bytes each */
-csp_buffer_init(10, 320);
-
-/* Init CSP with address 1 */
-csp_init(1);
-
-/* Init the CAN interface with hardware filtering */
-csp_can_init(CSP_CAN_MASKED, &can_conf)
-
-/* Setup default route to CAN interface */
-csp_route_set(CSP_DEFAULT_ROUTE, &csp_can_tx, CSP_HOST_MAC);
-
-/* Start router task with 500 word stack, OS task priority 1 */
-csp_route_start_task(500, 1);
-
-
-/*---------------------------Server------------------------------------------*/
-
-void csp_task(void *parameters) {
-    /* Create socket without any socket options */
-    csp_socket_t *sock = csp_socket(CSP_SO_NONE);
-
-    /* Bind all ports to socket */
-    csp_bind(sock, CSP_ANY);
-
-    /* Create 10 connections backlog queue */
-    csp_listen(sock, 10);
-
-    /* Pointer to current connection and packet */
-    csp_conn_t *conn;
-    csp_packet_t *packet;
-
-    /* Process incoming connections */
-    while (1) {
-        /* Wait for connection, 10000 ms timeout */
-        if ((conn = csp_accept(sock, 10000)) == NULL)
-            continue;
-
-        /* Read packets. Timout is 1000 ms */
-        while ((packet = csp_read(conn, 1000)) != NULL) {
-            switch (csp_conn_dport(conn)) {
-                case MY_PORT:
-                    /* Process packet here */
+                // read packets, timeout is 100 ms
+                while ((packet = csp_read(conn, 100)) != NULL) {
+            switch (csp_conn_dport(conn)) { // check the destination port of the connection
+                case 10:
+                    // when a message is sent to port 10, show the content
+                    printf("Packet from address %d: '%s'\n", 
+                           csp_conn_src(conn), (char *) packet->data);
+                    csp_buffer_free(packet); // free buffer for next message
+                    break;
                 default:
-                    /* Let the service handler reply pings, buffer use, etc. */
+                    // if it was sent to an uninteresting port, call normal service handler
                     csp_service_handler(conn, packet);
                     break;
             }
+                }
+                // close current connection
+                csp_close(conn);
         }
-
-        /* Close current connection, and handle next */
-        csp_close(conn);
-    }
+        return CSP_TASK_RETURN;
 }
+
+void csp_setup( uint8_t address ) {
+    printf("Setting up CSP with address: %d\n", address);
+    
+    // csp configuration and initialization
+    csp_conf_t csp_conf;
+    csp_conf_get_defaults(&csp_conf);
+    csp_conf.address = address;
+    csp_init(&csp_conf);
+
+    // initialize buffer
+    csp_buffer_init(5, 300);
+}
+
+void kiss_setup( char              * device
+               , char              * name
+               , csp_iface_t       * kiss_iface_p
+               , csp_kiss_handle_t * kiss_driver_p
+               , usart_callback_t    callback_func ) {
+    printf("Setting up KISS interface %s with device: %s\n", name, device);
+
+    // usart configuration
+    struct usart_conf conf;
+    conf.device = device;
+    conf.baudrate = 115200;
+    
+    usart_init(&conf);
+
+    csp_kiss_init(kiss_iface_p, kiss_driver_p, usart_putc,
+                  usart_insert, name);
+    
+    // function to catch incoming chars. 
+    usart_set_callback(callback_func);
+
+    // set route for kiss interface
+    csp_route_set(CSP_DEFAULT_ROUTE, kiss_iface_p, CSP_NODE_MAC);
+}
+
+void csp_start( csp_thread_return_t (* thread_task)(void *), char * name ) {
+    printf("Starting %s task\n", name);
+
+    // start router task with 1000 bytes of stack    
+    csp_route_start_task(1000, 0);
+
+    // create the thread with our task
+    csp_thread_handle_t thread_handle;
+    csp_thread_create(thread_task, name, 1000, NULL, 0, &thread_handle);
+}
+
+void show_debug() {
+    printf("Debug enabled\r\n");
+        csp_debug_toggle_level(3);
+        csp_debug_toggle_level(4);
+
+        printf("Conn table\r\n");
+        csp_conn_print_table();
+
+        printf("Route table\r\n");
+        csp_route_print_table();
+
+        printf("Interfaces\r\n");
+        csp_route_print_interfaces();
+}
+
+int main(int argc, char * argv[]) {
+    // parse command line arguments
+    uint8_t address;                                                                         
+    char * kiss_device;                                                                      
+    if (argc == 3) {                                                                         
+        address = atoi(argv[1]);                                                             
+        kiss_device = argv[2];                                                               
+    }                                                                                        
+    else  {                                                                                  
+        printf("Usage:\n"                                                                    
+               " server <csp-id> <kiss-device>\n");                                          
+       exit(1);                                                                              
+    }                                                                                        
+                                                                                             
+    csp_setup(address);                                                                      
+                                                                                             
+    if (kiss_device != NULL) {                                                               
+        // initialize kiss interface (these values must be available for all of main function)                                                                                            
+        csp_iface_t kiss_iface;                                                              
+        csp_kiss_handle_t kiss_driver;                                                       
+        // callback function when receiving over kiss                                        
+        void kiss_callback_func(uint8_t * buf, int len, void * pxTaskWoken) {                
+            csp_kiss_rx(&kiss_iface, buf, len, pxTaskWoken);                                 
+        }                                                                                    
+        // setup kiss                                                                        
+        kiss_setup(kiss_device, "KISS interface", &kiss_iface, &kiss_driver, kiss_callback_func);                                                                                         
+    }                                                                                        
+                                                                                             
+    csp_start(task_server, "server");                                                        
+                                                                                             
+    //show_debug(); // uncomment this line for debug info                                    
+                                                                                             
+    while(1) {                                                                               
+        csp_sleep_ms(1000);                                                                  
+    }                                                                                        
+    return 0;                                                                                
+}    
